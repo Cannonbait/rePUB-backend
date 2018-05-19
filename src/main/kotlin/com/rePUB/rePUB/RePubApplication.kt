3389f45e20abd045ef4e84996c11b7d1da89eb35
Ivar Josefsson <ivar_busig@hotmail.com>
2018-05-19 08:37:11 +0200

    Initial spring commit

---
+++ src/main/kotlin/com/rePUB/rePUB/RePubApplication.kt
@@ -0,0 +1,11 @@
+package com.rePUB.rePUB
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RePubApplication
+
+fun main(args: Array<String>) {
+    runApplication<RePubApplication>(*args)
+}
---
+++ src/test/kotlin/com/rePUB/rePUB/RePubApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rePUB.rePUB
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RePubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
